11faec2d7c4a5af925b1c4566f900e6ac28457e8
Kancherla, Rohan <rohan360k@gmail.com>
2025-07-28 18:19:45 -0500

    Telepath

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,12 +1,16 @@
 return {
   {
     'ggandor/leap.nvim',
-    opts = {
-      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)'),
-      vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)'),
-      vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)'),
-      vim.keymap.set({ 'x', 'o' }, 'x', '<Plug>(leap-forward-till)'),
-      vim.keymap.set({ 'x', 'o' }, 'X', '<Plug>(leap-backward-till)'),
-    },
+    opts = function()
+      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+      vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+      vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
+      vim.keymap.set({ 'x', 'o' }, 'x', '<Plug>(leap-forward-till)')
+      vim.keymap.set({ 'x', 'o' }, 'X', '<Plug>(leap-backward-till)')
+      local preview_filter = function(ch0, ch1, ch2)
+        return not (ch1:match '%s' or ch0:match '%a' and ch1:match '%a' and ch2:match '%a')
+      end
+      return { preview_filter }
+    end,
   },
 }
